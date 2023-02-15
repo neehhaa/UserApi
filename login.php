27c02a500d620fa694aa037d0f05f4dafa3af3fa
neehhaa <nehawasnik95@outlook.com>
2023-02-15 19:10:05 +0530

    fixing login with email

--- login.php
+++ login.php
@@ -5,16 +5,29 @@
     $email = $_POST['email'];
     $password = md5($_POST['password']);
 
-    $checkUser = "SELECT * FROM user WHERE email = '$email' and password = '$password'";
+    $checkUser = "SELECT * FROM user WHERE email = '$email'";
     $result = mysqli_query($conn,$checkUser);
 
     if(mysqli_num_rows($result) > 0){
-        $response['error'] = "000";
-        $response['message'] = "LOGIN SUCCESS";
-    }
-    else{
-        $response['error'] = "001";
-        $response['message'] = "LOGIN FAILED";
+        $checkUserQuery = "SELECT id, username, email FROM user WHERE email = '$email' and password = '$password'";
+        $resultant = mysqli_query($conn,$checkUserQuery);
+
+        if(mysqli_num_rows($resultant) > 0){
+            while($row = $resultant -> fetch_assoc()){
+                $response['user'] = $row;
+            }
+            $response['error'] = "200";
+            $response['message'] = "LOGIN SUCCESS";
+        }
+        else{
+            $response['user'] = (object)[];
+            $response['error'] = "400";
+            $response['message'] = "WRONG CREDENTIALS";
+        }
+    }else{
+        $response['user'] = (object)[];
+        $response['error'] = "400";
+        $response['message'] = "USER DONT EXIST";
     }
 
     echo json_encode($response);
