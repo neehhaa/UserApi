935163855bd4fda91270649ebb9d9d519d3620ad
neehhaa <nehawasnik95@outlook.com>
2023-02-15 17:16:33 +0530

    fetching all users

---
+++ fetchusers.php
@@ -0,0 +1,18 @@
+<?php
+
+    require 'connection.php';
+
+    $users = "SELECT username, email FROM user";
+    $result = mysqli_query($conn, $users);
+
+    if(mysqli_num_rows($result) > 0){
+
+        while($row = $result -> fetch_assoc()){
+            $response['users'][] = $row;
+        }
+    }
+    else{
+        $response['error'] = "400";
+    }
+    echo json_encode($response);
+?>
